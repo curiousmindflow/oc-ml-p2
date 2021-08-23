a9861dca1db72936a2f9c618f313060ffc14b29f
Alex <alexandre.falher.pro@gmail.com>
2021-08-23 07:59:34 +0200

    nutriscore formula writing in progress

---
+++ scripts/nutriscore.py
@@ -0,0 +1,13 @@
+def calculate(row):
+    # P
+    fvn = 0 # fruits, vegetables, nuts
+    fiber = 0
+    protein = 0
+
+    # N
+    energy = 0
+    sugar = 0
+    sat_fat = 0
+    sodium = 0
+
+    
